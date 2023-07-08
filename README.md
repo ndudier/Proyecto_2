7490abef0107de5f53fc1ae213a02d136a9b3fcc
ndudier <131488939+ndudier@users.noreply.github.com>
2023-07-08 19:07:24 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# Proyecto_01. Desarrollo de mi proyecto de HTML5 Y CSS  como actividad de la Comisión 11 de Codigo Pa Lante.  
---
+++ js/main.js
@@ -0,0 +1,7 @@
+menuitems.forEach(function(item){
+    item.addEventListener('click', function(e) {
+        const currentItem = document.querySelector('.active');
+        currentItem.classList.remove('active');
+        e.target.classList.add('active');
+    });
+});
